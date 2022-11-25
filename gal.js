754dc23b9120c8351837f339535b2b450a8f6cd8
Betty Afolabi <betty4web@gmail.com>
2022-11-25 16:41:30 +0100

    gallery test

---
+++ gal.js
@@ -0,0 +1,82 @@
+const change = document.getElementById('imgjs')
+
+change.addEventListener('click', (event) =>{
+    event.target.style.background = 'white';
+    event.target.style.color = 'green';
+})
+const change2 = document.getElementById('imgjs1')
+
+change2.addEventListener('click', (event) =>{
+    event.target.style.background = 'black';
+    event.target.style.color = 'white';
+    event.target.style.width = '316px';
+})
+const change3 = document.getElementById('imgjs2')
+
+change3.addEventListener('click', (event) =>{
+    event.target.style.background = 'white';
+    event.target.style.color = 'blue';
+})
+const change4 = document.getElementById('imgjs3')
+
+change4.addEventListener('click', (event) =>{
+    event.target.style.background = 'brown';
+    event.target.style.color = 'black';
+    event.target.style.width = '316px';
+})
+const change5 = document.getElementById('imgjs4')
+
+change5.addEventListener('click', (event) =>{
+    event.target.style.background = 'blue';
+    event.target.style.color = 'black';
+    event.target.style.width = '316px';
+})
+const change6 = document.getElementById('imgjs5')
+
+change6.addEventListener('click', (event) =>{
+    event.target.style.background = 'green';
+    event.target.style.color = 'white';
+    event.target.style.width = '316px';
+})
+const change7 = document.getElementById('imgjs6')
+
+change7.addEventListener('click', (event) =>{
+    event.target.style.background = 'black';
+    event.target.style.color = 'blue';
+    event.target.style.width = '316px';
+})
+const change8 = document.getElementById('imgjs7')
+
+change8.addEventListener('click', (event) =>{
+    event.target.style.background = 'rgb(41, 19, 1)';
+    event.target.style.color = 'pink';
+    event.target.style.width = '316px';
+})
+const change9 = document.getElementById('imgjs8')
+
+change9.addEventListener('click', (event) =>{
+    event.target.style.background = 'grey';
+    event.target.style.color = 'red';
+    event.target.style.width = '316px';
+})
+const change10 = document.getElementById('imgjs9')
+
+change10.addEventListener('click', (event) =>{
+    event.target.style.background = 'green';
+    event.target.style.color = 'rgb(41, 19, 1)';
+    event.target.style.width = '316px';
+})
+const change0 = document.getElementById('imgjs0')
+
+change0.addEventListener('click', (event) =>{
+    event.target.style.background = 'black';
+    event.target.style.color = 'yellow';
+    event.target.style.width = '316px';
+})
+const changei = document.getElementById('imgjsi')
+
+changei.addEventListener('click', (event) =>{
+    event.target.style.background = 'rgb(41, 19, 1)';
+    event.target.style.color = 'red';
+    event.target.style.width = '316px';
+})
